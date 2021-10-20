63dd7a562c48fd8c11cdb68ed8d747e0c2245e58
PelleLindroth <pellelindroth@gmail.com>
2021-10-20 14:28:19 +0200

    Removed comment

--- medium/multiTables.js
+++ medium/multiTables.js
@@ -1,5 +1,3 @@
-// Write a program that prints a multiplication table for numbers up to 12.
-
 const printMultiplicationTable = () => {
   for (let i = 1; i <= 12; i++) {
     for (let j = 1; j <= 10; j++) {
