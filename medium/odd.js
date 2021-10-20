be2c9b1eb17de3534dd93241df1d013234b0299a
rutbergphilip <philiprutberg00@gmail.com>
2021-10-20 13:45:14 +0200

    returning odd numbers only

--- medium/odd.js
+++ medium/odd.js
@@ -4,7 +4,9 @@ const readline = require("readline-sync")
 const firstPrompt = readline.question('Give me a list of numbers: '); // eg. 1,2,3
 const numbers = firstPrompt.split(',').map(x => parseInt(x));
 
+const result = numbers.filter(number => number % 2 !== 0);
+
 // Your code here...
 
-console.log('The odd numbers are:' + '...');
+console.log('The odd numbers are:', result);
 
