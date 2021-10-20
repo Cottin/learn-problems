b424a5b1468bd733e7351b7444c824cbc9296073
PelleLindroth <pellelindroth@gmail.com>
2021-10-20 13:36:00 +0200

    Write printMultiplicationTable function

--- medium/multiTables.js
+++ medium/multiTables.js
@@ -1,7 +1,14 @@
 // Write a program that prints a multiplication table for numbers up to 12.
 
-// Your code here...
+const printMultiplicationTable = () => {
+  for (let i = 1; i <= 12; i++) {
+    for (let j = 1; j <= 10; j++) {
+      console.log(`${j} * ${i} = ${i * j}`)
+    }
+    console.log('---------------------------------')
+  }
+}
 
-console.log('Multiplication tables up to 12:');
+printMultiplicationTable()
 
 
