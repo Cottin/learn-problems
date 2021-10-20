588569f59de65e7ac7ad01490e32f86b66056d6b
splarsxd <splarsiz34@yahoo.com>
2021-10-20 11:35:14 +0200

    Update reverse.js

--- medium/reverse.js
+++ medium/reverse.js
@@ -2,9 +2,6 @@ const readline = require("readline-sync")
 
 // Write function that reverses a list of numbers
 const firstPrompt = readline.question('Give me a list of numbers: '); // eg. 1,2,3
-const numbers = firstPrompt.split(',').map(x => parseInt(x));
+const reversed = firstPrompt.split('').reverse().join('')
 
-// Your code here...
-
-console.log('The reverse list is:' + '...');
-
+console.log('The reverse list is: ' + reversed);
