dbb2af514c0840767f851e5c2e59d80a470bf7f9
PelleLindroth <pellelindroth@gmail.com>
2021-10-20 11:23:27 +0200

    Add pigLatin translation

--- medium/pigLatin.js
+++ medium/pigLatin.js
@@ -3,10 +3,15 @@ const readline = require("readline-sync")
 // Write function that translates a text to Pig Latin and back. English is translated to Pig Latin by taking 
 // the first letter of every word, moving it to the end of the word and adding ‘ay’. “The quick brown fox” 
 // becomes “Hetay uickqay rownbay oxfay”.
-const firstPrompt = readline.question('Give me a sentence: '); // eg. The quick brown fox
-const words = firstPrompt.split(' ');
+const firstPrompt = readline.question('Give me a sentence: ') // eg. The quick brown fox
+const words = firstPrompt.split(' ')
 
-// Your code here...
+pigWords = []
 
-console.log('Translated:' + '...');
+words.forEach(word => {
+  word = word.slice(1) + word[0] + 'ay'
+  pigWords.push(word)
+})
+
+console.log('Translated:' + pigWords.join(' '))
 
