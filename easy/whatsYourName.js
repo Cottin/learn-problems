827b0723bdb7facc827134d4a749b3a54b1eb70b
John Doe <soold.simon@gmail.com>
2021-10-20 11:53:56 +0200

    ojdå

--- easy/whatsYourName.js
+++ easy/whatsYourName.js
@@ -3,7 +3,9 @@ const readline = require("readline-sync")
 // Write a program that asks the user for their name and greets them with their name.
 const firstPrompt = readline.question('...? ');
 
+console.log(firstPrompt)
+
 // Your code here...
 
-console.log('...' + '...');
+console.log('Hello ' + firstPrompt);
 
--- hard/guessNum.js
+++ hard/guessNum.js
@@ -3,7 +3,19 @@ const readline = require("readline-sync")
 // Write a guessing game where the user has to guess a secret number. After every guess the program tells 
 // the user whether their number was too large or too small. At the end the number of tries needed should 
 // be printed. It counts only as one try if they input the same number multiple times consecutively.
-const firstPrompt = readline.question('What do you guess? ');
+const number = Math.floor(Math.random() * 99)
+let truth = false 
+while (!truth) {
+    let firstPrompt = readline.question('What do you guess? ');
+
+if (firstPrompt < number) {
+    console.log("to small")
+} else if (firstPrompt > number) {
+    console.log("to big")
+} else {
+    truth = true
+}
+}
 
 // Your code here...
 
